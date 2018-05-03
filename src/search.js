78ca20ed29bc8a841d6db3e70024ab3e41ba5aee
Ian Hazelton <ian.hazelton@gmail.com>
2018-05-03 15:48:49 +0200

    Adding classes to elements & styling

--- src/search.js
+++ src/search.js
@@ -6,8 +6,12 @@ const render = () => {
   const body = document.getElementsByTagName('body')[0]
   const input = document.createElement('input')
   const button = document.createElement('button')
+  const wrapInput = document.createElement('div')
 
-  input.setAttribute('type', 'text')
+  input.setAttribute('type', 'search')
+  input.setAttribute('class', 'searchInput')
+  input.setAttribute('type', 'search')
+  input.setAttribute('placeholder', 'Write here')
   input.addEventListener('input', evt => {
     const { value } = evt.target
     button.disabled = value.length === 0
@@ -15,12 +19,16 @@ const render = () => {
 
   button.disabled = true
   button.textContent = 'Search'
-  button.addEventListener('click', evt => {
+  button.setAttribute('class', 'searchButton')
+  button.addEventListener('click', () => {
     search(input.value)
   })
 
-  body.appendChild(input)
-  body.appendChild(button)
+  wrapInput.setAttribute('class', 'wrapInput')
+  wrapInput.appendChild(input)
+  wrapInput.appendChild(button)
+
+  body.appendChild(wrapInput)
 }
 
 export default render
